54d8b786a47b10addff73be3b9d42f26a57f0815
seenivasan <seeni0424@gmail.com>
2018-01-13 20:56:49 +0530

    Modified CreadtePickle

--- Pickles/CreatePickle.py
+++ Pickles/CreatePickle.py
@@ -6,19 +6,12 @@ import scipy.misc
 from skimage.color import rgb2gray
 from skimage.transform import resize
 import sys
-#limit execution based on where you run
-#1 Creates the Dataset.pkl for all images else only 1000
-flag=True if (int(sys.argv[1])==1) else False
-print("Whether Data is for all:",flag)
-input("Press key to continue:")
+
 #root directory
 root_directory="tamil_dataset_offline"
+copy_root_directory="Pkl/"
 #get all the user_directory in the directory
 users_directory=os.listdir(root_directory)
-
-# define images and labels
-images=[]
-labels=[]
 
 #image characterist
 image_size=100
@@ -33,12 +26,23 @@ def hotfixLabel(n):
 
 
 total_captured=0;
+flag=True
 for user in users_directory:
+	#check user specifed a  directory size
+	if(flag and user!=sys.argv[1]):
+		continue
+	if(user==sys.argv[1]):
+		flag=False
+
 	print("Processing Directory:"+str(user))
-	#extract label and image from the files in usrt_direcotory
+	# define images and labels
+	images=[]
+	labels=[]
+
+	#extract label and image from the files in uset_direcotory
 	for file in os.listdir(root_directory+"/"+user+"/"):
 		#check for other types of files excluding .tiff images
-		print(file)
+		#print(file)
 		try:
 			#do a dummy operation that would exec error on notOkayfiles
 			label=int(file[:3])
@@ -51,33 +55,21 @@ for user in users_directory:
 			image=image[:,:,:3] # remove alpha channel
 		image=rgb2gray(image) # remove rgb traces
 		#no need as normalixation depends on the whole dataset and it can be done in trainer side
-		image=(image-pixel_depth/2)/pixel_depth # do normalization
+		#image=(image-pixel_depth/2)/pixel_depth # do normalization
 		image=resize(image,(image_size,image_size)) #resize image to image_size,image_size
-		print(np.array(image).shape)
+		#print(np.array(image).shape)
 		images.append(image)
 		labels.append(hotfixLabel(label))
 		total_captured+=1
 		print("Count:",total_captured)
-	if(not flag and total_captured>500):
-		break
-		
-images=np.array(images)
-labels=np.array(labels)
-print("Images shape ",images.shape)
-print("Labels shape ",labels.shape)
-#let us print the mean and deviation
-print("No of images:"+str(len(images)))
-print("mean:"+str(np.mean(images)))
-print("Standard deviation:"+str(np.std(images)))
-
-#store the dataset in pickelfile
-pickel_file="Dataset.pkl"
-save={
+	save={
 	"images":images,
 	"labels":labels
-}
-
-file_p=open(pickel_file,'wb')
-pickle.dump(save,file_p)
-
-print("Dataset Dumped Suceesfully")
+	}
+	pickel_file=copy_root_directory+user+".pkl"
+	file_p=open(pickel_file,'wb')
+	pickle.dump(save,file_p)
+	file_p.close()
+	print("Saved in ",pickel_file)
+		
+print("Whole User Dataset Dumped Suceesfully")
