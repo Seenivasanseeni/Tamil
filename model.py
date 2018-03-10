364d8b2d062ac87045173ced25f2023697ae3df5
SeenivasanSeeni <seeni0424@gmail.com>
2018-03-10 06:20:08 -0800

    # layer model with accuracy if 10%

--- model.py
+++ model.py
@@ -16,18 +16,22 @@ class Model(object):
         self.label=tf.placeholder(dtype=tf.float32,shape=[None,num_characters])
         
         #layer1
-        layer1=tf.layers.dense(self.image,units=1024,activation=tf.nn.relu)
+        layer0=tf.layers.dense(self.image,units=num_characters,activation=tf.nn.relu)
         
-        layer2=tf.layers.dense(layer1,units=num_characters,activation=tf.nn.relu)
+        layer1=tf.layers.dense(layer0,units=1024,activation=tf.nn.relu)
         
-        logits=tf.nn.softmax(layer2)
+        layer2=tf.layers.dense(layer1,units=512,activation=tf.nn.relu)
+        
+        layer3=tf.layers.dense(layer2,units=self.num_characters,activation=tf.nn.relu)
+        
+        logits=tf.nn.softmax(layer3)
 
         self.loss=tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=self.label,logits=logits))
 
         self.accuracy=tf.reduce_mean(tf.cast(tf.equal(tf.argmax(self.label,1),tf.argmax(logits,1)),tf.float32))
         self.learning_rate=0.5
 
-        self.optimizer=tf.train.GradientDescentOptimizer(self.learning_rate).minimize(self.loss)
+        self.optimizer=tf.train.AdamOptimizer(self.learning_rate).minimize(self.loss)
         self.sess=tf.InteractiveSession()
         tf.global_variables_initializer().run()
         tf.local_variables_initializer().run()
